5f68a66a51a84ea9ca7e00a131b0fb8cddb96bb6
华晨江 <huachenjiang@shxgroup.net>
2019-06-28 16:18:17 +0800

    1、新增OnFragmentResult注释

--- AndroidAnnotations/androidannotations-core/androidannotations-api/src/main/java/org/androidannotations/annotations/OnFragmentResult.java
+++ AndroidAnnotations/androidannotations-core/androidannotations-api/src/main/java/org/androidannotations/annotations/OnFragmentResult.java
@@ -5,15 +5,31 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * @see EFragment
+ */
+
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.METHOD)
 public @interface OnFragmentResult {
-
+	/**
+	 * The <b>requestCode</b> associated with the given result.
+	 *
+	 * @return the requestCode
+	 */
 	int value();
 
+	/**
+	 * @see OnFragmentResult
+	 */
 	@Retention(RetentionPolicy.CLASS)
 	@Target(ElementType.PARAMETER)
 	@interface Extra {
+		/**
+		 * They key of the result data.
+		 *
+		 * @return the key
+		 */
 		String value() default "";
 	}
 }
